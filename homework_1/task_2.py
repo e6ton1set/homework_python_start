1c84c61441a397f14b30809c1459761bd2a8d001
Ruslan Karimov <ru-ka-@mail.ru>
2022-11-22 20:42:32 +0500

    added task_2

---
+++ homework_1/task_2.py
@@ -0,0 +1,10 @@
+
+x = int(input('Введите число X: '))
+y = int(input('Введите число Y: '))
+z = int(input('Введите число Z: '))
+
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
