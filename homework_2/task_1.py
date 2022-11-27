8f754dea3add717273ab82f6efbfe7ddd0c31788
Ruslan Karimov <ru-ka-@mail.ru>
2022-11-27 19:55:29 +0500

    added task_1 in homework_2

---
+++ homework_2/task_1.py
@@ -0,0 +1,12 @@
+# Напишите программу, которая принимает на вход вещественное число 
+# и показывает сумму его цифр.
+
+num = input('Введите вещественное число: ')
+
+def sum_float(num):
+    return sum(map(int, num.replace('.','').replace('-','').replace(',','')))
+
+result = sum_float(num)
+
+print(f'\nСумма цифр в числе {num} = {result}')
+
